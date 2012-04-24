7049bdecea3d2d3436aaaaf5174cfc6dbe8d1c3d
guillaume guerin <guillaume_guerin.dev@me.com>
2012-04-24 17:32:23 +0200

    add bootstrap…

---
+++ PhpBootstrap/PhpBootstrap/src/lib/Tag.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author : Guillaume Guerin
+ *
+ * Description : Html Tag Class
+ * 
+ * @license MIT
+ * @link https://github.com/GecTeam/PhpBootstrap
+ * @copyright GecTeam 2012
+ * @version 1.0
+ */
+
+namespace Library;
+
+class Tag {
+	
+	
+	
+}
+
+
+?>
