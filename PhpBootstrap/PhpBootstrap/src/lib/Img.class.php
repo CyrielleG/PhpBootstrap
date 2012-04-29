3dc1445d038553d03224b13b799d881e92352642
unknown <Cyrielle@.(none)>
2012-04-29 16:00:36 +0200

    Ajout de la classe Img Classe fonctionnelle

---
+++ PhpBootstrap/PhpBootstrap/src/lib/Img.class.php
@@ -0,0 +1,36 @@
+﻿<?php 
+
+/**
+ * 
+ * 
+ * @author Cyrielle&Eric
+ * Description : Html Img Class
+ * 
+ * @license MIT
+ * @link https://github.com/GecTeam/PhpBootstrap
+ * @copyright GecTeam 2012
+ * @version 1.0
+ */
+
+namespace Library;
+
+	class Img extends Tag {
+		
+		/**
+		 * 
+		 * @param $src string chemin de l'image ex: c://Users/Moi/Images/Img.jpg
+		 * @param $alt string texte alternatif à l'image ex: image de lion
+		 * @param $title string titre de l'image(optionnel) ex: Lion d'Afrique
+		 */
+		function __construct($src, $alt, $title = NULL)
+		{
+			if(is_string($title)) {
+				parent::__construct(img, array("src" => $src, "alt" => $alt,
+				"title" => $title), NULL, true);
+			} else {
+				parent::__construct(img, array("src" => $src, "alt" => $alt), 
+				NULL, true);
+			}
+		}	
+	}	
+?>
